5400be1a9bffb02011866c333c9339091d260ad8
Axel Álvarez <axelalvarezsantos@gmail.com>
2024-07-24 20:33:35 +0200

    Update Authentication with forgotpassword

---
+++ MyReads/MyReads/Authentication/AuthenticationViews/ForgetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgetPasswordView.swift
+//  MyReads
+//
+//  Created by Axel Álvarez Santos on 24/7/24.
+//
+
+import SwiftUI
+
+struct ForgetPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgetPasswordView()
+}
---
+++ MyReads/MyReads/GoogleApi/Repository/Config.swift
@@ -0,0 +1,8 @@
+//
+//  Config.swift
+//  MyReads
+//
+//  Created by Axel Álvarez Santos on 24/7/24.
+//
+
+import Foundation
